a927dbc7b2c5900c3acecca372cd886a3b7b242b
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-11-07 18:44:24 +0100

    Update TSQL2MD.sql

--- TSQL2MD.sql
+++ TSQL2MD.sql
@@ -29,3 +29,50 @@ SELECT * FROM TestForMD
 -- 3. add the headers and remove any annotations (SET NOCOUNT OFF and similar)
 -- 4. save the results to md file
 -- 5. open the file
+
+
+-- Select statement
+CREATE OR ALTER PROCEDURE dbo.Select2MD
+    @table_name VARCHAR(200)
+    ,@schema_name VARCHAR(20)
+AS 
+BEGIN
+    -- get the columns of the table
+        SELECT 
+            c.Column_name
+            ,c.Ordinal_position
+            ,c.is_nullable
+            ,c.Data_Type
+        FROM INFORMATION_SCHEMA.TABLES AS  t
+        JOIN INFORMATION_SCHEMA.COLUMNS AS c 
+        ON t.table_name = c.table_name
+        AND t.table_schema = c.table_schema
+        AND t.table_Catalog = c.table_Catalog
+        WHERE
+        t.table_type = 'BASE TABLE'
+        AND t.Table_name = @table_name
+        AND t.table_schema = @schema_name
+
+
+END;
+
+
+EXEC dbo.select2MD
+@table_name = 'TestForMD'
+,@schema_name = 'dbo'
+
+
+SELECT 
+    c.Column_name
+    ,c.Ordinal_position
+    ,c.is_nullable
+    ,c.Data_Type
+FROM INFORMATION_SCHEMA.TABLES AS  t
+JOIN INFORMATION_SCHEMA.COLUMNS AS c 
+ON t.table_name = c.table_name
+AND t.table_schema = c.table_schema
+AND t.table_Catalog = c.table_Catalog
+WHERE
+t.table_type = 'BASE TABLE'
+AND t.Table_name = 'TestForMD'
+
