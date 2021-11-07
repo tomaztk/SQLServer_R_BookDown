de389d94d61a9a83903beb45c2aaa6993bfcf4ba
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2021-11-07 22:13:36 +0100

    update

--- TSQL2MD.sql
+++ TSQL2MD.sql
@@ -43,6 +43,9 @@ BEGIN
             ,c.Ordinal_position
             ,c.is_nullable
             ,c.Data_Type
+        
+        INTO  #temp
+        
         FROM INFORMATION_SCHEMA.TABLES AS  t
         JOIN INFORMATION_SCHEMA.COLUMNS AS c 
         ON t.table_name = c.table_name
@@ -53,6 +56,32 @@ BEGIN
         AND t.Table_name = @table_name
         AND t.table_schema = @schema_name
 
+
+
+            DECLARE @MD NVARCHAR(MAX)
+
+            -- Title
+            DECLARE @title NVARCHAR(MAX) = (SELECT '##Result for table: _**' + CAST(@table_name AS NVARCHAR(MAX)) + '**_
+            ###SchemaName: _'+CAST(@schema_name AS NVARCHAR(MAX)) +'_')
+
+
+            -- header |name |name2 |name3 |name4 |name5 |name6 
+            DECLARE @header VARCHAR(MAX)
+
+            SELECT @header = COALESCE(@header + '**|**', '') + column_name 
+            FROM #temp
+            ORDER BY Ordinal_position ASC
+            SELECT @header = '|**' + @header + '**|'
+
+
+            -- delimiter |-- |-- |-- |-- |-- |-- 
+            DECLARE @nof_columns INT = (SELECT MAX(Ordinal_position) FROM #temp)
+            DECLARE @firstLine NVARCHAR(MAX) = (SELECT  REPLICATE('|---',@nof_columns) + '|')  
+
+
+            SET @MD = @title +CHAR(10) + @header + CHAR(13) + CHAR(10) + @firstLine
+
+            SELECT @MD
 
 END;
 
@@ -67,6 +96,9 @@ SELECT
     ,c.Ordinal_position
     ,c.is_nullable
     ,c.Data_Type
+
+INTO #temp
+
 FROM INFORMATION_SCHEMA.TABLES AS  t
 JOIN INFORMATION_SCHEMA.COLUMNS AS c 
 ON t.table_name = c.table_name
@@ -77,5 +109,40 @@ t.table_type = 'BASE TABLE'
 AND t.Table_name = 'TestForMD'
 
 
+DECLARE @MD NVARCHAR(MAX)
+
+-- Title
+DECLARE @title NVARCHAR(MAX) = SELECT '##Result for table: _**' + CAST(@table_name AS NVARCHAR(MAX)) + '**_
+###SchemaName: _'+CAST(@schema_name AS NVARCHAR(MAX)) +'_'
 
 
+-- header |name |name2 |name3 |name4 |name5 |name6 
+DECLARE @header VARCHAR(MAX)
+
+SELECT @header = COALESCE(@header + '**|**', '') + column_name 
+FROM #temp
+ORDER BY Ordinal_position ASC
+SELECT @header = '|**' + @header + '**|'
+
+
+-- delimiter |-- |-- |-- |-- |-- |-- 
+DECLARE @nof_columns INT = (SELECT MAX(Ordinal_position) FROM #temp)
+DECLARE @firstLine NVARCHAR(MAX) = (SELECT  REPLICATE('|---',@nof_columns) + '|')  
+
+
+SET @MD = @header + CHAR(13) + @firstLine
+
+SELECT @MD
+
+-- body
+DECLARE @body NVARCHAR(MAX)
+
+
+select 
+ '|' + CAST( id AS VARCHAR(MAX))+ '|' +
+CAST(name AS VARCHAR(MAX)) + '|' +
+CAST( age AS VARCHAR(MAX))+ '|' +
+CAST( salary AS VARCHAR(MAX))+ '|' +
+CAST( height AS VARCHAR(MAX))+ '|' +
+CAST( MaritalStatus AS VARCHAR(MAX))+ '|'
+ from dbo.testforMD
